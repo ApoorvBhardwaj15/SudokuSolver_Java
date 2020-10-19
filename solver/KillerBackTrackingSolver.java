c055b5d44b12dfbe7fbbaff78330351de4f74f09
ApoorvBhardwaj15 <62135647+ApoorvBhardwaj15@users.noreply.github.com>
2020-10-19 19:55:58 +1100

    Update KillerBackTrackingSolver.java

--- solver/KillerBackTrackingSolver.java
+++ solver/KillerBackTrackingSolver.java
@@ -1,12 +1,6 @@
-/*
- * @author Jeffrey Chan & Minyi Li, RMIT 2020
- */
-
 package solver;
-
 import java.util.ArrayList;
 import java.util.Set;
-
 import grid.KillerSudokuGrid.Cell;
 import grid.SudokuGrid;
 import grid.*;
@@ -22,34 +16,23 @@ public class KillerBackTrackingSolver extends KillerSudokuSolver
 		// TODO: any initialisation you want to implement.
 	} // end of KillerBackTrackingSolver()
 
-
 	@Override
 	public boolean solve(SudokuGrid Grid) {
 		// TODO: your implementation of a backtracking solver for Killer Sudoku.
 		int size =  Grid.getValues().length;
-		
-		
-		
-		
 		for (int row = 0; row < size; row++) {
 			for (int column = 0; column < size; column++) {
 				if (Grid.getGrid()[row][column] == 0) {
-					//System.out.println(row +","+column );
 					String cellId = row +"" +column; 
-				//	System.out.println(valueSet.contains(1)+"@@@");
 					for (int k = Grid.getValues()[0]; k <= Grid.getValues()[size-1]; k++) {
-					//	if(!valueSet.contains(k))
-						//	continue;
 						Grid.getGrid()[row][column] = k;
 						insertInCage(row,column,k); 
 						if (killerValidate(Grid,row,column) && solve(Grid)) {
 							return true;
 						}
 						deleteFromCage(row,column,0);
-						Grid.getGrid()[row][column] = 0;
-						
-					}
-					
+						Grid.getGrid()[row][column] = 0;						
+					}					
 					return false;
 				}
 			}
@@ -60,19 +43,15 @@ public class KillerBackTrackingSolver extends KillerSudokuSolver
 	
 	 
 	private void deleteFromCage(int row, int col, int k) {
-		// TODO Auto-generated method stub
 		String cellId = String.valueOf(row)+String.valueOf(col);
 		int cageId = grid.KillerSudokuGrid.getCellCageMapping().get(cellId);
 		ArrayList<Cell> cageList = grid.KillerSudokuGrid.getCages().get(cageId);
-		
 		for(int i = 0 ; i < cageList.size() ; i ++) {
 			if(cageList.get(i).label.compareTo(cellId)==0) {
 				cageList.get(i).value=k;
 			}
-		}
-		
+		}		
 	}
-
 
 	private void insertInCage(int row, int col, int k) {
 		// TODO Auto-generated method stub
@@ -87,23 +66,18 @@ public class KillerBackTrackingSolver extends KillerSudokuSolver
 		}
 	}
 
-
-	public boolean killerValidate(SudokuGrid grid, int row, int column) {
-		
+	public boolean killerValidate(SudokuGrid grid, int row, int column) {		
 		if(grid.validate( grid,  row,  column) && cageConstraint(row, column)){
 			return true;
-		}
-		
+		}	
 		return false;
 	}
-
 
 	private boolean cageConstraint(int row, int col) {
 		// TODO Auto-generated method stub
 		String cellId = String.valueOf(row)+String.valueOf(col);
 		int cageId = grid.KillerSudokuGrid.getCellCageMapping().get(cellId);
-		ArrayList<Cell> cageList = grid.KillerSudokuGrid.getCages().get(cageId);
-		
+		ArrayList<Cell> cageList = grid.KillerSudokuGrid.getCages().get(cageId);	
 		if(containsZero(cageList) >= 1) {
 			return checkPartialSum(cageList);
 		}
@@ -111,7 +85,6 @@ public class KillerBackTrackingSolver extends KillerSudokuSolver
 			return checkTotalSum(cageList);
 		}
 	}
-
 
 	private boolean checkTotalSum(ArrayList<Cell> cageList) {
 		// TODO Auto-generated method stub
@@ -124,7 +97,6 @@ public class KillerBackTrackingSolver extends KillerSudokuSolver
 		return false;
 	}
 
-
 	private boolean checkPartialSum(ArrayList<Cell> cageList) {
 		// TODO Auto-generated method stub
 		int sum=0;
@@ -136,7 +108,6 @@ public class KillerBackTrackingSolver extends KillerSudokuSolver
 		return false;
 	}
 
-
 	private int containsZero(ArrayList<Cell> cageList) {
 		// TODO Auto-generated method stub
 		int counter = 0;
@@ -144,8 +115,6 @@ public class KillerBackTrackingSolver extends KillerSudokuSolver
 			if(cageList.get(i).value==0)
 				counter++;
 		}
-		//System.out.println(counter);
-		
 		return counter;
 	}
 
