ced28ae18355f931b21be116ce954f26385a4b46
ApoorvBhardwaj15 <62135647+ApoorvBhardwaj15@users.noreply.github.com>
2020-10-19 19:54:04 +1100

    Update KillerAdvancedSolver.java

--- solver/KillerAdvancedSolver.java
+++ solver/KillerAdvancedSolver.java
@@ -1,44 +1,25 @@
-/*
- * @author Jeffrey Chan & Minyi Li, RMIT 2020
- */
-
 package solver;
 import java.util.List;
 import java.util.Stack;
 
 import grid.SudokuGrid;
 import grid.KillerSudokuGrid.Cell;
-
-
 /**
- * Your advanced solver for Killer Sudoku.
+ * Advanced solver for Killer Sudoku.
  */
 public class KillerAdvancedSolver extends KillerSudokuSolver
 {
-	// TODO: Add attributes as needed.
-
-	public KillerAdvancedSolver() {
-		// TODO: any initialisation you want to implement.
-	} // end of KillerAdvancedSolver()
-
-
 	@Override
 	public boolean solve(SudokuGrid Grid) {
 		// TODO: your implementation of your advanced solver for Killer Sudoku.
 		int size =  Grid.getValues().length;
-
-
-
-
 		for (int row = 0; row < size; row++) {
 			for (int column = 0; column < size; column++) {
 				if (Grid.getGrid()[row][column] == 0) {
 					String cellId = row +"" +column; 
 					int cageId = grid.KillerSudokuGrid.getCellCageMapping().get(cellId);
-					//System.out.println(row +","+column );
 					List<Cell> cellList =  grid.KillerSudokuGrid.cageList.get(cageId-1).getCells();
-					List<Stack<Integer>> permutatedSolutions  = grid.KillerSudokuGrid.cageList.get(cageId-1).permutatedSolutions;
-					
+					List<Stack<Integer>> permutatedSolutions  = grid.KillerSudokuGrid.cageList.get(cageId-1).permutatedSolutions;				
 					for( int sol = 0 ; sol < permutatedSolutions.size() ; sol++) {
 						Stack<Integer> cloned = (Stack<Integer>) permutatedSolutions.get(sol).clone();
 						for(int i =0 ; i < cellList.size(); i++) {
@@ -47,12 +28,10 @@ public class KillerAdvancedSolver extends KillerSudokuSolver
 						
 						if(validate(Grid,cellList) && solve(Grid)) {
 							return true;
-						}
-						
+						}					
 						for(int i =0 ; i < cellList.size(); i++) {
 							Grid.getGrid()[cellList.get(i).row][cellList.get(i).col] = 0;
-						}
-						
+						}					
 					}
 					return false;
 				}
@@ -75,8 +54,5 @@ public class KillerAdvancedSolver extends KillerSudokuSolver
 		}
 		return true;
 	}
-
-
-
 
 } // end of class KillerAdvancedSolver
