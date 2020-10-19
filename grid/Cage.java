1bb941d215bcf65fd6eca616e7efd73ed8699ec0
ApoorvBhardwaj15 <62135647+ApoorvBhardwaj15@users.noreply.github.com>
2020-10-19 19:45:09 +1100

    Update Cage.java

--- grid/Cage.java
+++ grid/Cage.java
@@ -6,41 +6,12 @@ import java.util.Stack;
 import grid.*;
 import grid.KillerSudokuGrid.Cell;
 
-/**
- * COURSE: CECS-551 AI
- * PROFESSOR: Todd Ebert
- * PROJECT: Killer Sudoku Solver
- *
- * A Cage is the grouping of cells in a Killer Sudoku puzzle denoted by a dotted
- * line or by individual colours.
- *
- * Cages contain 2 to 9 cells and a value that the cells in the cage should sum to.
- *
- * @author Kristin Peterson
- * @author Ariel Katz
- *
- */
 public class Cage {
 	
 	public int id;
 	private int sum;
 	private List<Cell> cellList;
 	public List<Stack<Integer>> permutatedSolutions;
-
-	/**
-	 * Cage constructor, takes an input string and
-	 * constructs Cage object
-	 *
-	 * Info format:
-	 * x,y,x,y,...,g
-	 *
-	 * where:
-	 * x == row
-	 * y == col
-	 * g == Cage sum goal
-	 *
-	 * @param info Cage info from raw text file
-	 */
 	public Cage(int id,int sum, List<Cell> cellList,List<Stack<Integer>> permutatedSolutions ){
 		this.id = id;
 		this.sum = sum;
@@ -48,30 +19,15 @@ public class Cage {
 		this.permutatedSolutions = new ArrayList<Stack<Integer>>();
 		permutePossibleSolutions(permutatedSolutions);
 	}
-
-	/**
-	 * Returns the sum goal for this cage
-	 *
-	 * @return the sum goal for this cage
-	 */
+	
 	public int getSum()  {
 		return sum;
 	}
 
-	/**
-	 * Returns a list of cells that comprise this cage
-	 *
-	 * @return a list of cells that the cage is comprised of
-	 */
 	public List<Cell> getCells() {
 		return cellList;
 	}
 
-	/**
-	 * Return the list of cells as an array of cells
-	 *
-	 * @return the list of cells as an array of cells
-	 */
 	public Cell[] getCellsAsArray() {
 		int numCells = this.cellList.size();
 		Cell[] cells = new Cell[numCells];
@@ -81,12 +37,6 @@ public class Cage {
 		return cells;
 	}
 
-	/**
-	 * Sets the possible solutions for Cells in this Cage
-	 * and permutates possible Cage solutions
-	 *
-	 * @param ps possible solutions for Cells in this Cage
-	 */
 	public void setSolutions(List<Stack<Integer>> ps){
 		// Permutate solutions, populates Cage.permutatedSolutions
 		permutePossibleSolutions(ps);
@@ -101,11 +51,6 @@ public class Cage {
 		}
 	}
 
-	/**
-	 * Returns a copy of the list of all permuated possible solutions for this Cage
-	 *
-	 * @return permutated list of all possible solutions for this Cage
-	 */
 	public ArrayList<ArrayList<Integer>> getPermutatedSolutions(){
 		ArrayList<ArrayList<Integer>> solutionsClone = new ArrayList<ArrayList<Integer>>();
 		for (Stack<Integer> s : permutatedSolutions) {
@@ -118,11 +63,6 @@ public class Cage {
 		return solutionsClone;
 	}
 
-	/**
-	 * Finds all unique permutations of each solution Stack
-	 *
-	 * @param ps set of possible solutions that will be permutated
-	 */
 	private void permutePossibleSolutions(List<Stack<Integer>> ps) {
 		for (int i = 0; i < ps.size(); i++) {
 			permuteStack(ps.get(i), 0);
@@ -152,24 +92,5 @@ public class Cage {
 		}
 	}
 
-	/**
-	 * Builds a string representation of the cage, which identifies
-	 * the cage's cells (x & y value) and the sum goal
-	 *
-	 * @return a string representation of the cage
-	 */
-	public String toString() {
-		return "";
-	}
-
-	/**
-	 * A string representation of the cage
-	 * [cage-id] + [cage-cells] + [sum-goal] + [list of permutated possible solutions]
-	 *
-	 * @return a string representation of the cage
-	 */
-	public String toStringWithSolutions() {
-		return "";
-	}
 
 }
